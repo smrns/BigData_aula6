17b5f7b08b95e45198df8a47adb0f0420c8e6d24
smrns <sabrinamarins06@gmail.com>
2026-04-14 20:45:00 -0300

    Exercicio usando o for e o range

---
+++ atividade01.py
@@ -0,0 +1,8 @@
+for a in range(10):
+    print(f'\nAluno {a+1}') 
+    n1 = float(input('Digite a 1º nota: '))
+    n2 = float(input('Digite a 2º nota: '))
+    n3 = float(input('Digite a 3º nota: '))
+    n4 = float(input('Digite a 4º nota: '))
+    media = (n1 + n2 + n3 + n4)/4
+    print(f'A média do Aluno é: {media}')
